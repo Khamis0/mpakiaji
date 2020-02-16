1e9e80c2a2d4eb4c35fb7d87a42b7dc92a447550
Khamis08 <39904290+Khamis08@users.noreply.github.com>
2020-02-16 14:45:41 +0300

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -165,11 +165,7 @@ async def youtube_dl_call_back(bot, update):
     ad_string_to_replace = "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output."
     if e_response and ad_string_to_replace in e_response:
         error_message = e_response.replace(ad_string_to_replace, "")
-        await bot.edit_message_text(
-            chat_id=update.message.chat.id,
-            message_id=update.message.message_id,
-            text=error_message
-        )
+        
         return False
     if t_response:
         # logger.info(t_response)
