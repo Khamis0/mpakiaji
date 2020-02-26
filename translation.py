c5e1fa1a6758fa0a1ffde280bd9ac4c05c77cca5
Khamis08 <39904290+Khamis08@users.noreply.github.com>
2020-02-26 12:17:58 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -33,7 +33,7 @@ URL | filename | username | password"""
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
-    NO_VOID_FORMAT_FOUND = "Nimeshindwa kuifanyia Kazi link hii Jaribu kuituma kwa bot hawa @AnyDLBot @utubebot @uploadbot ..\n<b>YouTubeDL</b> said: {}"
+    NO_VOID_FORMAT_FOUND = "Nimeshindwa kuifanyia Kazi link url hii Jaribu kuituma kwa bot hawa 👉 @AnyDLBot @utubebot @uploadbot: \n {}"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     CURENT_PLAN_DETAILS = """Current plan details
 --------
