c4daa7fd9b5809182ffcc09bda7b58bf7d61dee4
Khamis08 <39904290+Khamis08@users.noreply.github.com>
2020-03-10 09:03:40 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -81,3 +81,15 @@ async def upgrade(bot, update):
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
     )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["hamis"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/hamis")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HAMIS_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
