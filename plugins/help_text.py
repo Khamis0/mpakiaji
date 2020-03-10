be06807214f1ee08887702a57dc03f3b9f7e3ddd
Khamis08 <39904290+Khamis08@users.noreply.github.com>
2020-03-10 09:05:30 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -88,7 +88,7 @@ async def upgrade(bot, update):
     TRChatBase(update.from_user.id, update.text, "/hamis")
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.HAMIS_TEXT,
+        text=Translation.HAMIS_MAJIBU,
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
